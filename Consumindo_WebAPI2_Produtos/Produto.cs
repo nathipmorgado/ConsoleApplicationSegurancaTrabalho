a5fc76da9b0943fcfb63721eedf201558d7d7c4c
nathy <nathaliapmorgado@gmail.com>
2024-01-26 10:22:59 -0300

    primeiro commit

---
+++ Consumindo_WebAPI2_Produtos/Produto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Consumindo_WebAPI2_Produtos
+{
+    internal class Produto
+    {
+    }
+}
---
+++ Consumindo_WebAPI2_Produtos/Program.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        await ConsumeApiAsync();
+    }
+
+    static async Task ConsumeApiAsync()
+    {
+        using (HttpClient client = new HttpClient())
+        {
+            // Substitua a URL pela URL real da API que você está consumindo
+            string apiUrl = "http://localhost:4444/api/v1/Colaborador";
+
+            try
+            {
+                HttpResponseMessage response = await client.GetAsync(apiUrl);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string responseData = await response.Content.ReadAsStringAsync();
+                    dynamic jsonResponse = JsonConvert.DeserializeObject(responseData);
+                    Console.WriteLine(jsonResponse); ;
+                }
+                else
+                {
+                    Console.WriteLine($"Erro na solicitação: {response.StatusCode}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Erro: {ex.Message}");
+            }
+        }
+    }
+}
